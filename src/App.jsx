2712ac8a16a5f79f8d4176d30689b5c9a10e62ac
zaidhanif1 <zah00005@mix.wvu.edu>
2026-07-28 18:43:32 -0400

    Register project detail page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import ByteBistroPage from './pages/projects/ByteBistro.jsx'
+import MPXOSPage from './pages/projects/MPXOS.jsx'
+import MomentumPage from './pages/projects/Momentum.jsx'
 
 export default function App()
 {
@@ -9,6 +11,8 @@ export default function App()
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/projects/bytebistro" element={<ByteBistroPage />} />
+                <Route path="/projects/mpx-os" element={<MPXOSPage />} />
+                <Route path="/projects/momentum" element={<MomentumPage />} />
             </Routes>
         </BrowserRouter>
     )
